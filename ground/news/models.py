40313f11a195102c5bbf3d94d3a206a51825a5a1
Chris <christoph.kralj@gmail.com>
2016-08-03 13:37:29 +0200

    Added tweet model

--- ground/news/models.py
+++ ground/news/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
+
 # Create your models here.
+class Tweet(models.Model):
+    # Basic tweet info
+    tweet_id = models.BigIntegerField()
+    text = models.CharField(max_length=250)
+    truncated = models.BooleanField(default=False)
+    lang = models.CharField(max_length=9, null=True, blank=True, default=None)
+
+    # Basic user info
+    user_id = models.BigIntegerField()
+    user_screen_name = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=150)
+    user_verified = models.BooleanField(default=False)
+
+    # Timing parameters
+    created_at = models.DateTimeField(db_index=True)  # should be UTC
+    user_utc_offset = models.IntegerField(null=True, blank=True, default=None)
+    user_time_zone = models.CharField(max_length=150, null=True, blank=True, default=None)
+
+    # none, low, or medium
+    filter_level = models.CharField(max_length=6, null=True, blank=True, default=None)
+
+    # Geo parameters
+    latitude = models.FloatField(null=True, blank=True, default=None)
+    longitude = models.FloatField(null=True, blank=True, default=None)
+    user_geo_enabled = models.BooleanField(default=False)
+    user_location = models.CharField(max_length=150, null=True, blank=True, default=None)
+
+    # Engagement - not likely to be very useful for streamed tweets but whatever
+    favorite_count = models.PositiveIntegerField(null=True, blank=True)
+    retweet_count = models.PositiveIntegerField(null=True, blank=True)
+    user_followers_count = models.PositiveIntegerField(null=True, blank=True)
+    user_friends_count = models.PositiveIntegerField(null=True, blank=True)
+
+    # Relation to other tweets
+    in_reply_to_status_id = models.BigIntegerField(null=True, blank=True, default=None)
+    retweeted_status_id = models.BigIntegerField(null=True, blank=True, default=None)
+
+
