246ce776224cb5e3c9067634ee1accf5a5f47c8a
Chris <christoph.kralj@gmail.com>
2016-08-03 18:03:47 +0200

    Basic encapsulation for twitter stream handler

--- ground/Mining/twitter_miner.py
+++ ground/Mining/twitter_miner.py
@@ -2,6 +2,8 @@
 from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
+import tweepy
+
 
 # This is a basic listener that just prints received tweets to stdout.
 class StdOutListener(StreamListener):
@@ -12,17 +14,32 @@ class StdOutListener(StreamListener):
 
     def on_error(self, status):
         print(status)
+        if status == 420:
+            # returning False in on_data disconnects the stream
+            return False
 
 
-if __name__ == '__main__':
+class Twitter:
 
-    # This handles Twitter authetification and the connection to Twitter Streaming API
-    l = StdOutListener()
-    auth = OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    stream = Stream(auth, l)
+    def __init__(self, c_key, c_secret, a_token, a_secret):
+        self.c_key = c_key
+        self.c_secret = c_secret
+        self.a_token = a_token
+        self.a_secret = a_secret
 
-    # This line filter Twitter Streams to capture data by the keywords: 'python', 'javascript', 'ruby'
-    stream.filter(track=['python', 'javascript', 'ruby'])
+
+    def connect_twitter(self):
+        auth = OAuthHandler(self.c_key, self.c_secret)
+        auth.set_access_token(self.a_token, self.a_secret)
+        self.api = tweepy.API(auth)
+        print("connected")
+
+
+    def connect_to_stream(self):
+        listener = StdOutListener()
+        stream = Stream(self.api.auth, listener)
+
+        stream.filter(track=['python', 'javascript', 'ruby'])
+
 
 
--- ground/news/views.py
+++ ground/news/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Tweet
+from Mining.twitter_miner import Twitter
+
+from django.conf import settings
+
 
 
 def index(request):
@@ -16,6 +20,10 @@ def index(request):
 
 
 def overview(request):
+    twitter = Twitter(settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.ACCESS_TOKEN, settings.ACCESS_SECRET)
+    twitter.connect_twitter()
+    twitter.connect_to_stream()
+
     return HttpResponse("Twitter Stream")
 
 
