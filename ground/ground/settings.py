36ecadd1768b44ffe70902b5ab216465ca5a0113
Chris <christoph.kralj@gmail.com>
2016-08-03 11:21:42 +0200

    Created news APP, added first view and postgres db access.

--- ground/ground/settings.py
+++ ground/ground/settings.py
@@ -73,12 +73,13 @@ WSGI_APPLICATION = 'ground.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {'default': {'ENGINE': 'django.db.backends.postgresql_psycopg2',
+                         'NAME': 'scapes_db',
+                         'USER': 'scapes',
+                         'PASSWORD': 'scapes',
+                         'HOST': '',
+                         'PORT': ''}}
+
 
 
 # Password validation
